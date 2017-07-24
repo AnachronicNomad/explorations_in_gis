e96795184db5070094135f49b6450e565e56b577
anachronicnomad <anachronicnomad@gmail.com>
2017-07-24 14:20:15 -0400

    reimplemented reading the indicator section of GRIB msg into object

--- grib2/grib2_parser/grib2_parser.js
+++ grib2/grib2_parser/grib2_parser.js
@@ -12,9 +12,16 @@ fs.open(path.join(__dirname, process.argv[2]), 'r', (err, fd) => {
   try {
     if(err) { throw err; }
     else {
+      message = {};
       fs.read(fd, Buffer.allocUnsafe(16), 0, 16, 0, 
               (err, bytesRead, buffer) => { 
-              console.log(buffer);
+          message.indicator = {
+            'msg_type': buffer.toString('ascii', 0, 4),
+            'discipline': buffer.readUInt8(6),
+            'edition': buffer.readUInt8(7),
+            'msg_length': parseInt(buffer.toString('hex', 8, 16), 16),
+          };
+          console.log(message);
       });
     }
   } catch(err) {
