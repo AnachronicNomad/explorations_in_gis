3614e4e932cbd86573d81d708a669cc6d2ce4ae5
anachronicnomad <anachronicnomad@gmail.com>
2017-07-24 02:00:55 -0400

    removed 'binary-parser', it was not to be :'/

--- grib2/grib2_parser/grib2_parser.js
+++ grib2/grib2_parser/grib2_parser.js
@@ -1,27 +1,10 @@
 const fs = require('fs');
 const path = require('path');
-const Parser = require('binary-parser').Parser;
 
 const printUsage = (() => {
   console.error("usage: node grib2_parser.js <file>");
   process.exit(1);
 }); 
-
-const indicator_header = new Parser()
-  .string('msg_tag', {
-    encoding: 'ascii',
-    length: 4,
-  })
-  .array('padding', {
-    type: 'uint8',
-    length: 2,
-  })
-  .uint8('discipline')
-  .uint8('grib_edition')
-  .string('msg_length', {
-    encoding: 'hex',
-    length: 8,
-  });
 
 if(process.argv.length != 3) { printUsage(); }
 
@@ -31,9 +14,8 @@ fs.open(path.join(__dirname, process.argv[2]), 'r', (err, fd) => {
     else {
       fs.read(fd, Buffer.allocUnsafe(16), 0, 16, 0, 
               (err, bytesRead, buffer) => { 
-                console.log(indicator_header.parse(buffer))
-                // console.log(buffer);
-              });
+              console.log(buffer);
+      });
     }
   } catch(err) {
     if(err.code === 'ENOENT') { console.error("the file does not exist"); }
