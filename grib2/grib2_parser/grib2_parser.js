ef2e0aa842b6471c55ac09ae1ac25597eae76ac5
anachronicnomad <anachronicnomad@gmail.com>
2017-07-24 00:56:02 -0400

    added use of binary-parser from NPM, parser for indicator section

--- grib2/grib2_parser/grib2_parser.js
+++ grib2/grib2_parser/grib2_parser.js
@@ -1,11 +1,27 @@
 const fs = require('fs');
 const path = require('path');
-const util = require('util');
+const Parser = require('binary-parser').Parser;
 
 const printUsage = (() => {
   console.error("usage: node grib2_parser.js <file>");
   process.exit(1);
 }); 
+
+const indicator_header = new Parser()
+  .string('msg_tag', {
+    encoding: 'ascii',
+    length: 4,
+  })
+  .array('padding', {
+    type: 'uint8',
+    length: 2,
+  })
+  .uint8('discipline')
+  .uint8('grib_edition')
+  .string('msg_length', {
+    encoding: 'hex',
+    length: 8,
+  });
 
 if(process.argv.length != 3) { printUsage(); }
 
@@ -13,9 +29,11 @@ fs.open(path.join(__dirname, process.argv[2]), 'r', (err, fd) => {
   try {
     if(err) { throw err; }
     else {
-      fs.fstat(fd, (err, stats) => {
-        console.log(util.inspect(stats));
-      })
+      fs.read(fd, Buffer.allocUnsafe(16), 0, 16, 0, 
+              (err, bytesRead, buffer) => { 
+                console.log(indicator_header.parse(buffer))
+                // console.log(buffer);
+              });
     }
   } catch(err) {
     if(err.code === 'ENOENT') { console.error("the file does not exist"); }
