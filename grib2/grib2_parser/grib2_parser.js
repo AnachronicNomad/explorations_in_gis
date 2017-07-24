145e1fb2812d493466f3e665809e872645ff4b3d
anachronicnomad <anachronicnomad@gmail.com>
2017-07-23 21:11:05 -0400

    made changes upon further reading of documentation to ensure sync

--- grib2/grib2_parser/grib2_parser.js
+++ grib2/grib2_parser/grib2_parser.js
@@ -1,22 +1,22 @@
 const fs = require('fs');
-const path = require('path')
+const path = require('path');
+const util = require('util');
 
-var printUsage = (() => {
+const printUsage = (() => {
   console.error("usage: node grib2_parser.js <file>");
   process.exit(1);
 }); 
 
 if(process.argv.length != 3) { printUsage(); }
-
-fs.access(path.join(__dirname, process.argv[2]),
-          fs.constants.R_OK | fs.constants.F_OK,
-          (err) => {
-            if(err) {
-              console.error("file does not exist");
-              printUsage();
-            }
-            else {
-              console.log("Looks like I can access the file.");
-            }
-          })
-
+try {
+  fs.open(path.join(__dirname, process.argv[2]), 'r', (err, fd) => {
+    if(err) { throw err; }
+    else {
+      console.log(util.inspect(fs.fstatSync(fd)));
+    }
+  })
+} catch(err) {
+  if(err.code === 'ENOENT') { console.error("the file does not exist"); }
+  else { console.error(err) }
+  printUsage();
+}
