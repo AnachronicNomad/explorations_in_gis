56e3555c5d9f744ad95363700502f24cda368688
anachronicnomad <anachronicnomad@gmail.com>
2017-07-25 16:23:03 -0400

    figured out what was wrong with init offset I was reading from

--- grib2/grib2_parser/grib2_parser.js
+++ grib2/grib2_parser/grib2_parser.js
@@ -6,46 +6,46 @@ const printUsage = (() => {
   process.exit(1);
 }); 
 
-const parse_grib2 = function(fd, message) {
-  msg_buffer = Buffer.allocUnsafe(message.indicator.msg_length);
-  var dataBuffer = [ ];
-  var bytesRead = fs.readSync(fd, msg_buffer, 0, message.indicator.length, 0);
-  if(bytesRead == message.indicator.length) {
-    throw new Error("GRIB2 message was not read correctly from file.");
-    return;
-  }
+const parse_grib2 = function(msg_buffer, message) {
+  buffer = msg_buffer.slice(16, 32);
+  console.log(buffer);
 
-  identification_header = { 'offset': 16, 'length': null };
-  localuse_header = { 'offset': null, 'length': null };
-  griddefn_header = { 'offset': null, 'length': null };
-  proddefn_header = { 'offset': null, 'length': null };
-  datarepr_header = { 'offset': null, 'length': null };
-  bitmap_header = { 'offset': null, 'length': null };
-  data_header = { 'offset': null, 'length': null };
+  // if(bytesRead != message.indicator.length) {
+  //   console.warn(`bytes read: ${bytesRead}`);
+  //   throw new Error("GRIB2 message was not read correctly from file.");
+  //   return;
+  // }
 
-  var offset = 0;
-  message.identification = {
-    'length': msg_buffer.readUInt32BE((offset=identification_header.offset)),
-    'section': msg_buffer.readUInt8((offset += 4)),
-    'centre_id': msg_buffer.readUInt16BE((++offset)),
-    'subcenter_id': msg_buffer.readUInt16BE((offset += 2)),
-    'master_tables_version': msg_buffer.readUInt8((offset += 2)),
-    'local_tables_version': msg_buffer.readUInt8((++offset)),
-    'sig_ref_time': msg_buffer.readUInt8((++offset)),
-    'time': {
-      'year': msg_buffer.readUInt16BE((++offset)),
-      'month': msg_buffer.readUInt8((offset += 2)),
-      'day': msg_buffer.readUInt8((++offset)),
-      'hour': msg_buffer.readUInt8((++offset)),
-      'minute': msg_buffer.readUInt8((++offset)),
-      'second': msg_buffer.readUInt8((++offset)),
-    },
-    'production_status': msg_buffer.readUInt8((++offset)),
-    'processed_type': msg_buffer.readUInt8((++offset)),
-  }
+  // identification_header = { 'offset': 16, 'length': null };
+  // localuse_header = { 'offset': null, 'length': null };
+  // griddefn_header = { 'offset': null, 'length': null };
+  // proddefn_header = { 'offset': null, 'length': null };
+  // datarepr_header = { 'offset': null, 'length': null };
+  // bitmap_header = { 'offset': null, 'length': null };
+  // data_header = { 'offset': null, 'length': null };
 
-  console.log(message);
-  return [message, dataBuffer];
+  // var offset = 0;
+  // message.identification = {
+  //   'length': msg_buffer.readUInt32BE(16),
+  //   'section': msg_buffer.readUInt8(20),
+  //   'centre_id': msg_buffer.readUInt16BE(21),
+  //   'subcenter_id': msg_buffer.readUInt16BE(23),
+  //   'master_tables_version': msg_buffer.readUInt8(24),
+  //   'local_tables_version': msg_buffer.readUInt8(25),
+  //   'sig_ref_time': msg_buffer.readUInt8(26),
+  //   'time': {
+  //     'year': msg_buffer.readUInt16BE(27),
+  //     'month': msg_buffer.readUInt8(29),
+  //     'day': msg_buffer.readUInt8(30),
+  //     'hour': msg_buffer.readUInt8(31),
+  //     'minute': msg_buffer.readUInt8(32),
+  //     'second': msg_buffer.readUInt8(33),
+  //   },
+  //   'production_status': msg_buffer.readUInt8(34),
+  //   'processed_type': msg_buffer.readUInt8(35),
+  // }
+
+  return;
 }
 
 if(process.argv.length != 3) { printUsage(); }
@@ -59,7 +59,7 @@ fs.open(path.join(__dirname, process.argv[2]), 'r', (err, fd) => {
       
       //todo: add error handling for if this is not actually a GRIB2 message
 
-      var message = { 
+      message = { 
         'indicator': {
           'msg_type': buffer.toString('ascii', 0, 4),
           'discipline': buffer.readUInt8(6),
@@ -68,7 +68,10 @@ fs.open(path.join(__dirname, process.argv[2]), 'r', (err, fd) => {
         }
       }
 
-      parse_grib2(fd, message);
+      msg_buffer = Buffer.alloc(message.indicator.msg_length);
+      fs.readSync(fd, msg_buffer, 0, message.indicator.msg_length, 0);
+
+      parse_grib2(msg_buffer, message);
     }
   } 
   catch(err) { console.error(err); printUsage(); }
