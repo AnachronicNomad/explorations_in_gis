67bc32471a9239ba99c209d017c1d3741db83e0d
anachronicnomad <anachronicnomad@gmail.com>
2017-07-25 14:57:51 -0400

    created 'parse_grib2' function, started moving logic from fs.read callback

--- grib2/grib2_parser/grib2_parser.js
+++ grib2/grib2_parser/grib2_parser.js
@@ -6,30 +6,39 @@ const printUsage = (() => {
   process.exit(1);
 }); 
 
+const parse_grib2 = function(fd, message) {
+  var msg_buffer = Buffer.allocUnsafe(message.indicator.length);
+  var dataBuffer = [ ];
+  var bytesRead = fs.readSync(fd, msg_buffer, 0, message.indicator.length, 0);
+  if(bytesRead == message.indicator.length) {
+    throw new Error("An individual GRIB2 message was not read correctly.");
+    return;
+  }
+  
+  return [message, dataBuffer];
+}
+
 if(process.argv.length != 3) { printUsage(); }
 
 fs.open(path.join(__dirname, process.argv[2]), 'r', (err, fd) => {
   try {
     if(err) { throw err; }
     else {
-      message = {};
       fs.read(fd, Buffer.allocUnsafe(16), 0, 16, 0, 
-              (err, bytesRead, buffer) => { 
-          message.indicator = {
-            'msg_type': buffer.toString('ascii', 0, 4),
-            'discipline': buffer.readUInt8(6),
-            'edition': buffer.readUInt8(7),
-            'msg_length': parseInt(buffer.toString('hex', 8, 16), 16),
-          };
-          console.log(message);
-      });
-    }
-  } catch(err) {
-    if(err.code === 'ENOENT') { console.error("the file does not exist"); }
-    else { console.error(err) }
-    printUsage();
-  } finally {
-    fs.close(fd, () => {process.exit()});
-  }
-})
+        (err, bytesRead, buffer) => { 
+          var message = { 
+            'indicator': {
+              'msg_type': buffer.toString('ascii', 0, 4),
+              'discipline': buffer.readUInt8(6),
+              'edition': buffer.readUInt8(7),
+              'msg_length': parseInt(buffer.toString('hex', 8, 16), 16),
+            }
+          }
+          parse_grib2(fd, message);
+        });
+      }
+  } 
+  catch(err) { console.error(err); printUsage(); }
+  finally { fs.close(fd, () => {process.exit()}); }
+});
 
