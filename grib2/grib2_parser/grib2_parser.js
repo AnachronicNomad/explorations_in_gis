01d571f2a0fc614398cc171a96611518a267388c
anachronicnomad <anachronicnomad@gmail.com>
2017-08-03 22:42:44 -0400

    changed name convention, refactored to be more like lexer

--- grib2/grib2_parser/grib2_parser.js
+++ grib2/grib2_parser/grib2_parser.js
@@ -17,8 +17,21 @@ function printUsage() {
   process.exit(1);
 } 
 
-function parse_ident_sec (buffer) {
-  identification = {
+function expand_indicator (buffer, message) {
+  message = { 
+        'indicator': {
+          'msg_type': buffer.toString('ascii', 0, 4),
+          'discipline': buffer.readUInt8(6),
+          'edition': buffer.readUInt8(7),
+          'msg_length': parseInt(buffer.toString('hex', 8, 16), 16),
+        }
+      }
+
+  return expand_grib2_to_json(buffer.slice(16),message);
+}
+
+function expand_identification (buffer, message) {
+  message.identification = {
     'length': buffer.readUInt32BE(0),
     'section': buffer.readUInt8(4),
     'centre_id': buffer.readUInt16BE(5),
@@ -39,26 +52,12 @@ function parse_ident_sec (buffer) {
     // todo: add a "remainder" field with hex-string of the rest of section
   }
 
-  if(identification.length > SECTION_MIN_LENGTHS.IDENTIFICATION_MIN_LENGTH ) {
-    identification.remainder = 
-      buffer.toString('hex', 
-                      SECTION_MIN_LENGTHS.IDENTIFICATION_MIN_LENGTH, 
-                      buffer.length);
-  }
-  return identification; 
+  return expand_grib2_to_json(buffer.slice(message.identification.length),
+                              message);
 }
 
-function parse_localuse_sec (buffer) {
-  local_use = {
-    'length': buffer.readUInt32BE(0),
-    'section': buffer.readUInt8(4),
-  }
-  local_use.remainder = buffer.toString('hex', 5, local_use.length);
-  return local_use;
-}
-
-function parse_gridDef_sec (buffer) {
-  grid_def = {
+function expand_grid_def (buffer, message) {
+  message.grid_def = {
     'length': buffer.readUInt32BE(0),
     'section': buffer.readUInt8(4),
     'source': buffer.readUInt8(5),
@@ -68,113 +67,83 @@ function parse_gridDef_sec (buffer) {
     'template_num': buffer.readUInt16BE(12),
   }
 
-  if(grid_def.length > SECTION_MIN_LENGTHS.GRID_DEF_MIN_LENGTH) {
-    grid_def.remainder = 
-      buffer.toString('hex',
-                      SECTION_MIN_LENGTHS.GRID_DEF_MIN_LENGTH, 
-                      buffer.length);
-  }
-  return grid_def;
+  return expand_grib2_to_json(buffer.slice(message.grid_def.length),
+                              message);
 }
 
-function parse_prodDef_sec (buffer) {
-  prod_def = {
+function expand_prod_def (buffer, message) {
+  message.prod_def = {
     'length': buffer.readUInt32BE(0),
     'section': buffer.readUInt8(4),
     'num_coord_values': buffer.readUInt16BE(5),
     'template_num': buffer.readUInt16BE(7),
   }
 
-  if(prod_def.length > SECTION_MIN_LENGTHS.PROD_DEF_MIN_LENGTH) {
-    prod_def.remainder = 
-      buffer.toString('hex',
-                      SECTION_MIN_LENGTHS.PROD_DEF_MIN_LENGTH,
-                      buffer.length);
-  }
-  return prod_def;
+  return expand_grib2_to_json(buffer.slice(message.prod_def.length),
+                              message);
 }
 
-function parse_datarepr_sec (buffer) {
-  data_repr = { 
+function expand_datarepr (buffer, message) {
+  message.data_repr = { 
     'length': buffer.readUInt32BE(0),
     'section': buffer.readUInt8(4),
     'num_data_points': buffer.readUInt32BE(5),
     'template_num': buffer.readUInt16BE(9),
   }
 
-  if(data_repr.length > SECTION_MIN_LENGTHS.GRID_DEF_MIN_LENGTH) {
-    data_repr.remainder = 
-      buffer.toString('hex',
-                      SECTION_MIN_LENGTHS.DATA_REPR_MIN_LENGTH, 
-                      buffer.length);
-  }
-  return data_repr;
+  return expand_grib2_to_json(buffer.slice(message.data_repr.length),
+                              message);
 }
 
-function parse_bitmap_sec (buffer) {
-  bit_map = {
+function expand_bitmap (buffer, message) {
+  message.bit_map = {
     'length': buffer.readUInt32BE(0),
     'section': buffer.readUInt8(4),
     'indicator': buffer.readUInt8(5),
-    'bitmap' : buffer.toString('hex', 
-                               SECTION_MIN_LENGTHS.BITMAP_MIN_LENGTH, 
-                               buffer.length)
   }
-  return bit_map;
+  message.bit_map.bitmap = buffer.toString('hex',
+                                           6,
+                                           message.bit_map.length);
+
+  return expand_grib2_to_json(buffer.slice(message.bit_map.length),
+                              message);
 }
 
-function parse_data_sec (buffer) {
-  data = {
+function expand_data (buffer, message) {
+  message.data = {
     'length': buffer.readUInt32BE(0),
     'section': buffer.readUInt8(4),
     'data': buffer.slice(5).toString('base64'),
   }
-  return data;
+
+  return expand_grib2_to_json(buffer.slice(message.data.length),
+                              message);
 }
 
-function parse_grib2 (msg_buffer, message) {
-  offset = 0;
-  if (message != null) { offset = SECTION_MIN_LENGTHS.INDICATOR_MIN_LENGTH; }
-  buffer = msg_buffer.slice(offset);
+function expand_grib2_to_json (buffer, message) {
+  if (buffer.length <= SECTION_MIN_LENGTHS.SECTION_MIN_LENGTH) {
+    return JSON.stringify(message, null, ' ');
+  }
 
-  while(buffer.length > SECTION_MIN_LENGTHS.SECTION_MIN_LENGTH) {
-    length = buffer.readUInt32BE(0);
-    section = buffer.readUInt8(4);
+  if( (buffer.toString('ascii', 0, 4)) == 'GRIB' )
+    { return expand_indicator(buffer, message); }
 
-    switch(section) {
-      case 1: 
-        message.identification = parse_ident_sec(buffer.slice(0, length)); 
-        break;
-      // case 2: 
-      //   message.local_use = parse_localuse_sec(buffer.slice(0, length));
-      //   break;
-      //todo: add proper handling for Local Use sections
-      case 3: 
-        message.grid_def = parse_gridDef_sec(buffer.slice(0, length));
-        break;
-      case 4:
-        message.prod_def = parse_prodDef_sec(buffer.slice(0, length));
-        break;
-      case 5:
-        message.data_repr = parse_datarepr_sec(buffer.slice(0, length));
-        break;
-      case 6:
-        message.bit_map = parse_bitmap_sec(buffer.slice(0, length));
-        break;
-      case 7:
-        message.data = parse_data_sec(buffer.slice(0, length));
-        break;
+  length = buffer.readUInt32BE(0);
+  section_num = buffer.readUInt8(4);
+
+  switch(section_num) {
+      case 1: return expand_identification(buffer, message);
+      case 3: return expand_grid_def(buffer, message);
+      case 4: return expand_prod_def(buffer, message);
+      case 5: return expand_datarepr(buffer, message);
+      case 6: return expand_bitmap(buffer, message);
+      case 7: return expand_data(buffer, message);
       default:
         console.error("lol I can't read wtf this is.");
         break;
     }
 
-    buffer = buffer.slice(length);
-  }
-
-  // console.log(message);
-  console.log(JSON.stringify(message, null, ' '));
-  return message;
+  // return JSON.stringify(message, null, ' ');;
 }
 
 if(process.argv.length != 3) { printUsage(); }
@@ -183,34 +152,17 @@ fs.open(path.join(__dirname, process.argv[2]), 'r', (err, fd) => {
   try {
     if(err) { throw err; }
     else {
-      buffer = Buffer.allocUnsafe(16);
-      fs.readSync(fd, buffer, 0, 16, 0);
-      
-      //todo: add error handling for if this is not actually a GRIB2 message
-
-      message = { 
-        'indicator': {
-          'msg_type': buffer.toString('ascii', 0, 4),
-          'discipline': buffer.readUInt8(6),
-          'edition': buffer.readUInt8(7),
-          'msg_length': parseInt(buffer.toString('hex', 8, 16), 16),
-        }
-      }
-
-      msg_buffer = Buffer.alloc(message.indicator.msg_length);
-      fs.read(fd, msg_buffer, 0, message.indicator.msg_length, 0, 
+      file_size = fs.fstatSync(fd).size;
+      fs.read(fd, 
+              Buffer.allocUnsafe(file_size), 
+              0, 
+              file_size, 
+              0, 
               (err, bytesRead, buffer) => { 
-                if(bytesRead != message.indicator.msg_length) {
-                  // console.warn(`bytes read: ${bytesRead}`);
-                  throw new Error(
-                    `An individual GRIB2 message was not read 
-                    correctly from file.`);
-                  return;
-                }
-                parse_grib2(buffer, message); 
-              });
+                  console.log(expand_grib2_to_json(buffer, {}));
+                });
     }
-  } 
+  }
   catch(err) { console.error(err); printUsage(); }
   finally { fs.close(fd, () => {process.exit()}); }
 });
